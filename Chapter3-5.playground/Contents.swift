15379f7f660d319c4d4e397add86686c81dfce29
Takahiro Hara <takahiro.hb@gmail.com>
2020-11-18 01:49:31 +0900

    start combine Chapter3-5

---
+++ Chapter3-5.playground/Contents.swift
@@ -0,0 +1,26 @@
+import Combine
+import Foundation
+
+let url = URL(string: "https://ww.example.com")!
+let publisher = URLSession.shared.dataTaskPublisher(for: url)
+
+final class Receiver {
+    var subscriptions = Set<AnyCancellable>()
+    
+    init() {
+        publisher
+            .sink(receiveCompletion: { completion in
+                if case let .failure(error) = completion {
+                    print("Received error:", error)
+                } else {
+                    print("Received completion:", completion)
+                }
+            }, receiveValue: { data, response in
+                print("Received data:", data)
+                print("Received response:", response)
+            })
+            .store(in: &subscriptions)
+    }
+}
+
+let receiver = Receiver()
